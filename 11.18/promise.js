6af8e2045562473ec3aab9af5b6e07b4a1236086
amor <2436264523@qq.com>
2022-11-18 22:07:10 +0800

    修改了

---
+++ 11.18/promise.js
@@ -0,0 +1,36 @@
+//    const fs=require("fs");
+//         const p=new Promise((resolve, reject) => {
+//             fs.readFile('./为学.md',(err,data)=>{
+//                 if (err) return reject(err);
+//                 resolve(data);
+//             })
+//         })
+//         p.then(function(value){
+//             console.log(value.toString());
+//         },function(err) {
+//             console.log('读取失败');
+//         })
+//    const fs=require("fs");
+//         const p=new Promise((resolve, reject) => {
+//             fs.readFile('./为学.md',(err,data)=>{
+//                 if (err) return reject(err);
+//                 resolve(data);
+//             })
+//         })
+//         p.then(function(value){
+//             console.log(value.toString());
+//         },function(err) {
+//             console.log('读取失败');
+//         })
+//    const fs=require("fs");
+//         const p=new Promise((resolve, reject) => {
+//             fs.readFile('./为学.md',(err,data)=>{
+//                 if (err) return reject(err);
+//                 resolve(data);
+//             })
+//         })
+//         p.then(function(value){
+//             console.log(value.toString());
+//         },function(err) {
+//             console.log('读取失败');
+//         })
