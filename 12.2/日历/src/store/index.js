b8ca55001fcdbd4df829d6a414d49bde2bf8b5c2
amor <2436264523@qq.com>
2022-12-03 22:45:57 +0800

    修改

--- 12.2/日历/src/store/index.js
+++ 12.2/日历/src/store/index.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import pers from 'vuex-persistedstate'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  plugins:[pers()],
   state: {
     list:[],
     check:false
@@ -11,10 +12,9 @@ export default new Vuex.Store({
   getters: {
   },
   mutations: {
-    add(state,obj) {
-    // if(state.data==obj.time){
-      state.list.push(obj)
-    // }
+    submitForm(state,obj) {
+    state.list.push(obj)
+    console.log(state.list);
     }
   },
   actions: {
--- 12.2/日历/src/views/HomeView.vue
+++ 12.2/日历/src/views/HomeView.vue
@@ -5,15 +5,25 @@
         >新增todolist</el-button
       >
       <el-calendar>
-        <template slot="dateCell" slot-scope="{ date, data }">
+        <template slot="dateCell" slot-scope="{ data }">
           <div>
             <p :class="data.isSelected ? 'is-selected' : ''">
               {{ data.day.split("-").slice(2).join("-") }}
             </p>
             <div>
-              <div v-for="item in $store.state.list" :key="item.id" v-show="data.day==item.time">
-                  <div class="green">{{item.name}}</div>
-                  <div class="red">{{item.name}}</div>
+              <div v-for="item in $store.state.list" :key="item.id">
+                <div
+                  class="green"
+                  v-if="item.time == data.day && item.resource == '已办'"
+                >
+                  {{ item.name }}
+                </div>
+                <div
+                  class="red"
+                  v-if="item.time == data.day && item.resource == '待办'"
+                >
+                  {{ item.name }}
+                </div>
               </div>
             </div>
           </div>
@@ -22,40 +32,40 @@
     </div>
 
     <el-dialog title="添加事件" :visible.sync="dialogFormVisible">
-      <el-form ref="form" :model="form" label-width="80px">
-        <el-form-item label="事情名称">
-          <el-input v-model="form.name"></el-input>
+      <el-form
+        :model="ruleForm"
+        :rules="rules"
+        ref="ruleForm"
+        label-width="100px"
+        class="demo-ruleForm"
+      >
+        <el-form-item label="时间名称" prop="name">
+          <el-input v-model="ruleForm.name"></el-input>
         </el-form-item>
 
-        <el-form-item label="待办时间">
+        <el-form-item label="活动时间" required>
           <el-col :span="11">
-            <el-date-picker
-              type="date"
-              placeholder="选择日期"
-              v-model="form.date1"
-              style="width: 100%"
-            ></el-date-picker>
-          </el-col>
-          <el-col class="line" :span="2">-</el-col>
-          <el-col :span="11">
-            <el-time-picker
-              placeholder="选择时间"
-              v-model="form.date2"
-              style="width: 100%"
-            ></el-time-picker>
+            <el-form-item prop="date1">
+              <el-date-picker
+                type="date"
+                placeholder="选择日期"
+                v-model="ruleForm.date1"
+                style="width: 100%"
+              ></el-date-picker>
+            </el-form-item>
           </el-col>
         </el-form-item>
-
-        <el-form-item label="事项进度">
-          <el-radio-group v-model="form.resource">
+        <el-form-item label="事项进度" prop="resource">
+          <el-radio-group v-model="ruleForm.resource">
             <el-radio label="待办"></el-radio>
             <el-radio label="已办"></el-radio>
           </el-radio-group>
         </el-form-item>
-
         <el-form-item>
-          <el-button type="primary" @click="add">立即创建</el-button>
-          <el-button>取消</el-button>
+          <el-button type="primary" @click="submitForm('ruleForm')"
+            >立即创建</el-button
+          >
+          <el-button>重置</el-button>
         </el-form-item>
       </el-form>
     </el-dialog>
@@ -67,28 +77,46 @@ export default {
   data() {
     return {
       dialogFormVisible: false,
-      form: {
+      ruleForm: {
         name: "",
-        date1: "",
-        date2: "",
+
         resource: "",
+        date1: "",
+      },
+      rules: {
+        name: [{ required: true, message: "请输入事项名称", trigger: "blur" }],
+        resource: [{ required: true, message: "请选择", trigger: "blur" }],
       },
     };
   },
   methods: {
-    add() {
-      let obj = {
-        id: new Date().getTime(),
-        name: this.form.name,
-        date1: this.form.date1,
-        date2: this.form.date2,
-        resource: this.form.resource,
-        time: new Date().getTime()
-      };
-      this.$store.commit("add", obj);
-      this.dialogFormVisible = false;
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          // alert("submit!");
+          let date = new Date(this.ruleForm.date1);
+          let year = date.getFullYear();
+          let month = date.getMonth() + 1;
+          month = month < 10 ? "0" + month : month;
+          let day = date.getDate();
+          day = day < 10 ? "0" + day : day;
+          let time = year + "-" + month + "-" + day;
+          let obj = {
+            id: new Date().getTime(),
+            name: this.ruleForm.name,
+            resource: this.ruleForm.resource,
+            time: time,
+          };
+          this.$store.commit("submitForm", obj);
+          this.dialogFormVisible = false;
+        } else {
+          // console.log("error submit!!");
+          return false;
+        }
+      });
     },
   },
+
   created() {},
   mounted() {},
   components: {},
