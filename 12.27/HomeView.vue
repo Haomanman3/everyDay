3f6fc5e848da18aef50f0ded3378019ea9d138e2
amor <2436264523@qq.com>
2022-12-27 23:17:47 +0800

    2201B-郝嫚嫚

---
+++ 12.27/HomeView.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class=''>
+      <p>{{point}}</p>
+      <p>{{point.x}}</p>
+      <p>{{point.y}}</p>
+    </div>
+</template>
+
+<script>
+import { ref, reactive, toRefs } from 'vue';
+import { useStore } from 'vuex';
+ import { useRouter, useRoute } from 'vue-router';
+ import usePoint from '../hooks/UserPointer.js'
+ export default {
+setup(props, ctx) {
+const store = useStore();
+const router = useRouter();
+const route = useRoute();
+const point=usePoint()
+console.log(point,"point");
+const data = reactive({});
+return { ...toRefs(data) ,point};
+ },
+ };
+</script>
+
+<style lang='scss' scoped>
+
+</style>
+
