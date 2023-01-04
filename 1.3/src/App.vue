59efde9abfc87500bc02bf3f5b55f52a52e0ac6b
amor <2436264523@qq.com>
2023-01-04 16:59:09 +0800

    2201B-郝嫚嫚

---
+++ 1.3/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+
+  <router-view/>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+*{
+  padding: 0;
+  margin: 0;
+  list-style: none;
+  box-sizing: border-box;
+}
+html,body,#app,.page{
+  height: 100%;
+
+}
+</style>
